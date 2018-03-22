81d49e9b86f8990b5f88eac62f74dcf8e1a298b7
Amin Bm <medaminbenmbarek@gmail.com>
2018-03-23 00:53:25 +0100

    Adding the cron job

--- helpers/PopularProfiles.js
+++ helpers/PopularProfiles.js
@@ -1,42 +1,41 @@
-const mongoose =require('mongoose');
 var Profile = require('../models/Profile');
 var popularProfile = require('../models/PopularProfile');
 var _ =require('lodash');
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('properties.file');
+var CronJob = require('cron').CronJob;
+
+var job=new CronJob('0 * * * * *', function() {
+    console.log('start');
+    Profile.find()
+        .sort({
+            nbLikes :-1
+        })
+        .limit(100)
+        .exec(function (err,docs) {
+            if (err){
+                return console.log(err);
+            }
+
+            popularProfile.remove({}).exec(function (err,result) {
+                if (err) {
+                    console.log(err);
+                }
+
+            });
 
 
-mongoose.connect(`mongodb://${properties.get('mongo.url')}/${properties.get('mongo.db.name')}`);
-var db =mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("connected!")
-});
+            _.map(docs ,function (doc) {
+                var Popular =new popularProfile(JSON.parse(JSON.stringify(doc)));
 
-
-Profile.find()
-    .sort({
-        nbLikes :-1
-    })
-    .limit(100)
-    .exec(function (err,docs) {
-    if (err){
-        return console.log(err);
-    }
-        db.dropCollection('popularprofiles', function(err, result) {
-            if (err) console.log('error while deleting',err);
+                Popular.save(err => {
+                    if (err) return console.log(err);
+                });
+            })
         });
 
-
-        _.map(docs ,function (doc) {
-
-         var Popular =new popularProfile(JSON.parse(JSON.stringify(doc)));
-
-        Popular.save(err => {
-            if (err) return console.log(err);
-    });
-    })
-    });
+},true,
+    'Tunisia');
 
 
 
