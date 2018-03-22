c66379a771b34fe093176150230fb4e931b83647
Amin Bm <medaminbenmbarek@gmail.com>
2018-03-22 23:24:36 +0100

    Update the file that creates the PopularProfiles collection (find then drop the old)

--- helpers/PopularProfiles.js
+++ helpers/PopularProfiles.js
@@ -2,6 +2,9 @@ const mongoose =require('mongoose');
 var Profile = require('../models/Profile');
 var popularProfile = require('../models/PopularProfile');
 var _ =require('lodash');
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('properties.file');
+
 
 mongoose.connect(`mongodb://${properties.get('mongo.url')}/${properties.get('mongo.db.name')}`);
 var db =mongoose.connection;
@@ -10,9 +13,6 @@ db.once('open', function() {
     console.log("connected!")
 });
 
-db.dropCollection('popularprofiles', function(err, result) {
-    if (err) console.log('error while deleting',err);
-});
 
 Profile.find()
     .sort({
@@ -23,7 +23,12 @@ Profile.find()
     if (err){
         return console.log(err);
     }
-    _.map(docs ,function (doc) {
+        db.dropCollection('popularprofiles', function(err, result) {
+            if (err) console.log('error while deleting',err);
+        });
+
+
+        _.map(docs ,function (doc) {
 
          var Popular =new popularProfile(JSON.parse(JSON.stringify(doc)));
 
