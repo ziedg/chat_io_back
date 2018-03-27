aacdb0d87c5f66a0a7f0af9b21f6fdd1187727d2
saidi zied <ziedsaidig@gmail.com>
2018-03-27 17:01:03 +0200

    add  gif to webp

--- routes/publicationRouter.js
+++ routes/publicationRouter.js
@@ -132,7 +132,7 @@ router.route('/publish')
                     console.log(filename);
                     sharp(Ofile)
                     .resize(1000)
-                    .webp()
+                    .toFormat(sharp.format.webp)
                     .toFile(`/var/www/html/images/${filename}`,(err)=>{
                        if(!err){
                         return fs.unlink(Ofile,(e)=>{
