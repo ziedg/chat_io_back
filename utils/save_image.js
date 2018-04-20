b0d869a86067ba5e602ffd132d86b9f8533b215d
zied <ziedsaidig@gmail.com>
2018-04-20 18:00:45 +0100

    refactor save image to be more reusable

---
+++ utils/save_image.js
@@ -0,0 +1,75 @@
+//librarry utile
+const sharp = require("sharp");
+const fs = require("fs");
+const client = require("scp2");
+var PropertiesReader = require("properties-reader");
+var properties = PropertiesReader("./properties.file");
+
+module.exports = (publication, files) => {
+  //const
+  const host = "173.249.14.90";
+  const username = "root";
+  const password = "MZ9xWqTJp5dS2teU";
+  path = properties.get("pictures.storage.folder").toString();
+  response = {
+    status: 0,
+    message: "PUBLISHED_SUCCESSFULLY",
+    publication: publication
+  };
+
+  if (files.publPicture) {
+    publication.publPictureLink = files.publPicture[0].filename;
+    var filePath = files.publPicture[0].path;
+    var destination = `${properties.get("pictures.storage.folder").toString() +
+      "/" +
+      files.publPicture[0].filename}`;
+    var ext = path.extname(req.files.publPicture[0].filename);
+    var filename = files.publPicture[0].filename;
+
+    if (ext.toLowerCase() !== ".gif") {
+      sharp(fimePath)
+        .resize(1000)
+        .toFile(destination)
+        .then(data => {
+          fs.unlink(filePath, err => {
+            if (!err) {
+              client.scp(
+                destination,
+                { host, username, password, path },
+                function(err) {
+                  if (!err) {
+                    fs.unlink(destination, err => {
+                      if (!err) {
+                        return response;
+                      } else {
+                        console.log(
+                          "error ocured when attempt to remove file ?"
+                        );
+                      }
+                    });
+                  }
+                  console.log("error ocurred in file transefert ?");
+                }
+              );
+            } else {
+              console.log("error ocured when attempt to remove file ?");
+            }
+          });
+        });
+    }
+
+    //cas fichier Gif
+    else {
+      client.scp(Ofile, { host, username, password, path }, function(err) {
+        if (!err) {
+          return response;
+        }
+        if (err) {
+          console.log("Error Occured in Gif transfert");
+        }
+      });
+    }
+  } else {
+    return response;
+  }
+};
