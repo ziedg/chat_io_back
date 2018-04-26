c9f16612a95ef54a4845f94b43c26c4780cb8a4b
zied <ziedsaidig@gmail.com>
2018-04-26 23:26:07 +0100

    updtae dev  prperties.file

--- routes/ProfileRouter.js
+++ routes/ProfileRouter.js
@@ -338,12 +338,12 @@ router.route('/updateProfilePicture')
                         }
 
 
-                         const link=  properties.get('pictures.link'); 
+                         const link=  properties.get('pictures.link').toString(); 
                         if (req.files.profilePicture[0]) {
                             profile.profilePicture =
-                                link + req.files.profilePicture[0].filename;
+                                link +'/'+ req.files.profilePicture[0].filename;
                             profile.profilePictureMin =
-                                link + req.files.profilePicture[0].filename;
+                                link +'/'+ req.files.profilePicture[0].filename;
 
 
                             //Update du photo dans les publications anterieures
@@ -369,9 +369,9 @@ router.route('/updateProfilePicture')
                                     if (!err){
                                         if (comment) {
                                             comment.profilePicture =
-                                                properties.get('pictures.link') + req.files.profilePicture[0].filename;
+                                                properties.get('pictures.link') +'/' +req.files.profilePicture[0].filename;
                                             comment.profilePictureMin =
-                                                properties.get('pictures.link') + req.files.profilePicture[0].filename;
+                                                properties.get('pictures.link') +'/'+ req.files.profilePicture[0].filename;
                                             comment.save();
                                         }
                                     }
