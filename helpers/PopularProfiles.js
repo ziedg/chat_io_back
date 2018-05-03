6c74be67053073536b2bae59a3dc3f7235d56674
Amin Bm <medaminbenmbarek@gmail.com>
2018-05-03 10:38:27 +0100

    cron chaque jour 5h du matin

--- helpers/PopularProfiles.js
+++ helpers/PopularProfiles.js
@@ -5,7 +5,7 @@ var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('properties.file');
 var CronJob = require('cron').CronJob;
 
-var job=new CronJob('0 */15 * * * *', function() {
+var job=new CronJob('0 0 5 * * *', function() {
     console.log('Lancement du cron');
     Profile.find()
         .sort({
