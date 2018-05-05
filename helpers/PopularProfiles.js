297a54ee154ea7ab96631e1bdbc771cf83a941d2
Administrator <admin@example.com>
2018-05-05 23:50:03 +0200

    Update PopularProfiles.js

--- helpers/PopularProfiles.js
+++ helpers/PopularProfiles.js
@@ -5,7 +5,7 @@ var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('properties.file');
 var CronJob = require('cron').CronJob;
 
-var job=new CronJob('0 0 5 * * *', function() {
+var job=new CronJob('0 0 23 * * *', function() {
     Profile.find()
         .sort({
             nbLikes :-1
