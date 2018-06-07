36fa3efbadb1f051d852a1bdc73e633230ed667b
touaiti <touaitiahmed@gmail.com>
2018-06-07 10:41:02 +0100

    socket cross first try

--- sockets/message.js
+++ sockets/message.js
@@ -3,6 +3,7 @@ const CONSTANTS=require('../utils/config/constants')
 var Message = require('../models/Message');
 
 module.exports = function(io){
+    io.set('origins', '*')
     io.use( async (socket, next) => {
         try {
             await socketQuery.addSocketId({
