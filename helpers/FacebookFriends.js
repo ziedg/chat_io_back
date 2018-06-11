de530f1244140efa9ad4c435b0303b7b81caf57a
saidi zied <ziedsaidig@gmail.com>
2018-06-11 04:00:15 +0100

    facebook friends

--- helpers/FacebookFriends.js
+++ helpers/FacebookFriends.js
@@ -1,28 +1,38 @@
 var Profile = require("../models/Profile");
 const _ = require("lodash");
 
-async function findByUserId(id, _id) {
+async function findByUserId(user,id) {
   const userFind = await Profile.findById(id);
-  if (userFind.subscribers.indexOf(String(_id)) === -1) return userFind;
+ 
+    
+  if (user.subscribers.indexOf(userFind._id) === -1)return userFind;
 }
 
 module.exports = {
   findfacebookFriends: async _id => {
     try {
+    
       const user = await Profile.findOne({ _id });
+     
+
       const friendsIds = user.friends;
       const facebookProfilesIds = await Profile.find(
         { facebookId: { $in: friendsIds } },
         "_id"
       );
+
+
       facebookfriends = await Promise.all(
-        facebookProfilesIds.map(async id => await findByUserId(id, _id))
+        facebookProfilesIds.map(async id => await findByUserId(user,id))
       );
+
+
 
       const facebookUniq = _.filter(
         facebookfriends,
         el => typeof el !== "undefined"
       );
+      
 
       return facebookUniq;
     } catch (error) {
