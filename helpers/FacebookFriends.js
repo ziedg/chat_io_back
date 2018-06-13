b0f28311c98df9bcd4433dbe6b035cffcd1b3cfb
saidi zied <ziedsaidig@gmail.com>
2018-06-13 23:32:15 +0100

    add ignored profile to facebook friends

--- helpers/FacebookFriends.js
+++ helpers/FacebookFriends.js
@@ -1,37 +1,34 @@
 var Profile = require("../models/Profile");
 const _ = require("lodash");
 
-async function findByUserId(user,id) {
+async function findByUserId(user, id) {
   const userFind = await Profile.findById(id);
- if (user.subscribers.indexOf(userFind._id) === -1)return userFind;
+  if (user.subscribers.indexOf(userFind._id) === -1) return userFind;
 }
 
 module.exports = {
-  findfacebookFriends: async (_id,page) => {
+  findfacebookFriends: async (_id, page) => {
     try {
-    
       const user = await Profile.findOne({ _id });
-     
 
       const friendsIds = user.friends;
       const facebookProfilesIds = await Profile.find(
-        { facebookId: { $in: friendsIds } },
+        {$and :[ {facebookId: { $in: friendsIds }},{_id:{$nin:user.ignoredProfiles}}]},
         "_id"
-      ).limit(10)
-      .skip((page-1)*10);
+      )
+        .limit(10)
+        .skip((page - 1) * 10);
 
+      
 
       facebookfriends = await Promise.all(
-        facebookProfilesIds.map(async id => await findByUserId(user,id))
+        facebookProfilesIds.map(async id => await findByUserId(user, id))
       );
-
-
 
       const facebookUniq = _.filter(
         facebookfriends,
         el => typeof el !== "undefined"
       );
-      
 
       return facebookUniq;
     } catch (error) {
