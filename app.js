69c03d6327874de25755d0444baba3549ab9e1fd
saidi zied <ziedsaidig@gmail.com>
2018-07-21 13:57:23 +0100

    update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
   var origin = req.headers.origin;
   if (allowedOrigins.indexOf(origin) > -1) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  }*/
 
   res.header(
     "Access-Control-Allow-Methods",
@@ -60,7 +60,7 @@ app.use(function(req, res, next) {
   res.setHeader(
     "Access-Control-Allow-Headers",
     "content-type,x-access-token,X-Requested-With"
-  );*/
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
